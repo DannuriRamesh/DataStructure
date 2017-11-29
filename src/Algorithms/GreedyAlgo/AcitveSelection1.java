652f123af8a3e5100d40c26f892280e53f44ed76
ramesh <rameshbabudannuri@gmail.com>
2017-11-29 20:32:49 +0530

    second commit

--- src/Algorithms/GreedyAlgo/AcitveSelection1.java
+++ src/Algorithms/GreedyAlgo/AcitveSelection1.java
@@ -1,6 +1,6 @@
 package Algorithms.GreedyAlgo;
 //sorted input
-public class AcitveSelection1 {
+    public class AcitveSelection1 {
     public static void main(String args[]){
         int s[] =  {1, 3, 0, 5, 8, 5};
         int f[] =  {2, 4, 6, 7, 9, 9};
